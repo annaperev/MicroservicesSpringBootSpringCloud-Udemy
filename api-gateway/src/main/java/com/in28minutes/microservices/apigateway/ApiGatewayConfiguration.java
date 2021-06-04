c1475b0be59c17e9fa30fb99624f813896e93e11
annaperev <perev13@gmail.com>
2021-06-04 08:56:06 +0200

    add routing and filters in gateway

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -1,28 +1,33 @@
 package com.in28minutes.microservices.apigateway;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-        Function<PredicateSpec, Buildable<Route>> routeFunction
-                = p -> p.path("/get") //if request comes to /get
-                .filters(f -> f
-                        .addRequestHeader("MyHeader", "MyURI")
-                        .addRequestParameter("Param","MyParam"))
-                .uri("http://httpbin.org:80"); //then we redirect to specific uri
         return builder.routes()
-                   .route(routeFunction)
+                   .route(p -> p.path("/get") //if request comes to /get
+                           .filters(f -> f
+                           .addRequestHeader("MyHeader", "MyURI")
+                           .addRequestParameter("Param","MyParam"))
+                           .uri("http://httpbin.org:80"))//then we redirect to specific uri
+                   .route(p -> p.path("/currency-exchange/**") //if request starts with  /currency-exchange/
+                           // then eureka will find the location of service "currency-exchange"
+                            .uri("lb://currency-exchange")) // "lb:  - this turn on load balance between instances of the services
+                   .route(p -> p.path("/currency-conversion/**")
+                           .uri("lb://currency-conversion"))
+                   .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                   .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath( //explained in 152 lecture https://www.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/learn/lecture/24346944#overview
+                                "currency-conversion-new/(?<segment>.*)"
+                                ,"currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
                    .build();
     }
 
